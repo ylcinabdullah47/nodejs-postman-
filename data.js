352bf77512920bb8d9c474797f8abd1aa2433cf8
abdullah <a.ylcin47@gmail.com>
2023-03-15 14:22:23 +0300

    git,post,delete put metotları eklendi delete dışında hepsi sorunsuz çalışıyor

--- data.js
+++ data.js
@@ -1,53 +1,17 @@
-// const ogrenciler= [
-//     {
-//         id:"1",
-//         ad_soyad:"ahmet bingül",
-//         mail:"ahmet@hotmail.com"
-//     },
-//     {
-//         id:"2",
-//         ad_soyad:"mustafa aktı",
-//         mail:"mustafa@hotmail.com"
-//     },
-//     {
-//         id:"3",
-//         ad_soyad:"emre fırça",
-//         mail:"emre@hotmail.com"
-//     },
-//     {
-//         id:"4",
-//         ad_soyad:"yusuf yazıcı",
-//         mail:"yusuf@hotmail.com"
-//     },
-//     {
-//         id:"5",
-//         ad_soyad:"mehmet ali",
-//         mail:"mehmet@hotmail.com"
-//     },
-//     {
-//         id:"6",
-//         ad_soyad:"",
-//         mail:""
-//     }
-//     // {
-//     //     id:"7",
-//     //     ad_soyad:"",
-//     //     mail:""
-//     // },
-//     // {
-//     //     id:"8",
-//     //     ad_soyad:"",
-//     //     mail:""
-//     // },
-//     // {
-//     //     id:"9",
-//     //     ad_soyad:"",
-//     //     mail:""
-//     // },
-//     // {
-//     //     id:"10",
-//     //     ad_soyad:"",
-//     //     mail:""
-//     // }
+const dataogrenci= [
 
-// ];
+
+    { id:1,ad_soyad:"ahmet bingül", mail:"ahmet@hotmail.com"},
+        
+    {id:2,ad_soyad:"mustafa aktı",mail:"mustafa@hotmail.com"},
+      
+    {id:3,ad_soyad:"emre fırça",mail:"emre@hotmail.com" },  
+         
+    {id:4,ad_soyad:"yusuf yazıcı",mail:"yusuf@hotmail.com"},   
+        
+    {id:5,ad_soyad:"mehmet ali", mail:"mehmet@hotmail.com"},
+       
+    {id:5,ad_soyad:"abdullah yalçın",mail:"abdullah@hotmail.com" },
+];
+
+module.exports=dataogrenci;
--- index.js
+++ index.js
@@ -1,93 +1,99 @@
-const express = require('express');
-// const data = require('./data.js')
-const server = express();
-const data = require('./ogrenciData.js')
-// const ogrenci= require('./ogrenci.json');
 
-// server.get("/",(req,res)=>{
-//     res.send("hoşgeldinizasdsad ")
-// });
 
-// server.get("/ogrenci",(req,res)=>{
-//     res.send("listele")
-//     res.status(200).json(ogrenciler);
+//      //BURASI TEST SAYFASI APP.JS KULLANILACAK
+
+
+
+// const express = require('express');
+// // const data = require('./data.js')
+// const server = express();
+// const data = require('./ogrenciData.js')
+// // const ogrenci= require('./ogrenci.json');
+
+// // server.get("/",(req,res)=>{
+// //     res.send("hoşgeldinizasdsad ")
+// // });
+
+// // server.get("/ogrenci",(req,res)=>{
+// //     res.send("listele")
+// //     res.status(200).json(ogrenciler);
     
  
-// });
+// // });
 
 
-// let next_id=4;
-//     server.post("/ogrenci",(req,res) =>{
-//     let yeni_ogrenci=req.body;
-//     yeni_ogrenci.id=next_id;
-//     next_id++;
-//     data.push(yeni_ogrenci);
-//     res.status(201).json(yeni_ogrenci);
-// })
+// // let next_id=4;
+// //     server.post("/ogrenci",(req,res) =>{
+// //     let yeni_ogrenci=req.body;
+// //     yeni_ogrenci.id=next_id;
+// //     next_id++;
+// //     data.push(yeni_ogrenci);
+// //     res.status(201).json(yeni_ogrenci);
+// // })
 
 
-// server.post("/posts",(req,res) =>{
-//     console.log(req.body);
-//     res.send(req.body);
-// })
+// // server.post("/posts",(req,res) =>{
+// //     console.log(req.body);
+// //     res.send(req.body);
+// // })
 
 
-// server.get("/ogrenci",(req,res)=>{
-//     res.send(ogrenci);
-// })
+// // server.get("/ogrenci",(req,res)=>{
+// //     res.send(ogrenci);
+// // })
 
 
-// boş dizi döndürüyor ekrana doğru veriler gelmiyor
+// // boş dizi döndürüyor ekrana doğru veriler gelmiyor
 
-// server.get("/data" ,(req,res)=>{
-//     res.send(data);
-// });
+// // server.get("/data" ,(req,res)=>{
+// //     res.send(data);
+// // });
 
 
-//yeni yöntem deneme1
+// //yeni yöntem deneme1
 
-// server.get("/data",(req,res)=>{
-//     const tableRows = data.map(
-//         (item) =>
-//           `<tr><td>${item.id}</td><td>${item.ad_soyad}</td><td>${item.mail}</td></tr>`
-//       );
-//       const table = `<table><tr><th>ID</th><th>Ad Soyad</th><th>Mail</th></tr>${tableRows.join(  ""   )}</table>`;
+// // server.get("/data",(req,res)=>{
+// //     const tableRows = data.map(
+// //         (item) =>
+// //           `<tr><td>${item.id}</td><td>${item.ad_soyad}</td><td>${item.mail}</td></tr>`
+// //       );
+// //       const table = `<table><tr><th>ID</th><th>Ad Soyad</th><th>Mail</th></tr>${tableRows.join(  ""   )}</table>`;
    
       
-//       res.send(table);
-//     });
+// //       res.send(table);
+// //     });
 
-//
+// //
 
-server.get('/',(req,res)=>{
-    res.send("sayfa sorunsuz çalışıyor2323232323")
-});
-
-
-server.get('/ogrenci',(req,res)=>{
-    // öğrenci listesini döndürmek için buraya kod eklenecek
-      res.send(data)
-
-});
-server.post('/ogrenci',(req,res)=>{
-    //yeni öğrenci kayıt etmek için buraya yazılacak
-    const yeniOgrenci =req.body;
-    console.log(yeniOgrenci)
-    res.send("yeni öğrenci kaydedildi");
-});
-server.put('/ogrenci/:id',(req,res)=>{
-    //öğrenci bilgi güncelleme
-
-});
-
-server.delete('/ogrenci/:id',(req,res)=>{
-    //öğrenci kayıt silme buraya yazılacak
-});
+// server.get('/',(req,res)=>{
+//     res.send("sayfa sorunsuz çalışıyor2323232323")
+// });
 
 
+// server.get('/ogrenci',(req,res)=>{
+//     // öğrenci listesini döndürmek için buraya kod eklenecek
+//       res.send(data)
+
+// });
+// server.post('/ogrenci',(req,res)=>{
+//     //yeni öğrenci kayıt etmek için buraya yazılacak
+//     const yeniOgrenci =req.body;
+//     console.log(yeniOgrenci)
+//     res.send("yeni öğrenci kaydedildi");
+// });
+// server.put('/ogrenci/:id',(req,res)=>{
+//     //öğrenci bilgi güncelleme
+
+// });
+// console.log("test");
+// server.delete('/ogrenci/:id',(req,res)=>{
+//     //öğrenci kayıt silme buraya yazılacak
+// });
 
 
-server.listen(3000,()=>{
+
+
+// server.listen(3000,()=>{
    
-    console.log("3000 portu çalışıyor");
-});
+//     console.log("3000 portu çalışıyor");
+// });
